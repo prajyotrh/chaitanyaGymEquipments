05f466f79cdbaa0a79d5164af739de628255545e
unknown <prajyothalingale05@gmail.com>
2020-12-13 21:50:50 +0530

    new items added

--- php/send.php
+++ php/send.php
@@ -6,7 +6,7 @@
 	$number = $_POST['number'];
 	$msg = $_POST['msg'];
 
-	$to_email = "prajyothalingale05@gmail.com";
+	$to_email = "chaitanyagym20@gmail.com";
 	$subject = "Enquiry related to gym equipments";
 
     $body = "Name :". $fname;
